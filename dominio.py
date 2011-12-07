be6defe4056169d025f82aeae4cf82b3c2688c12
Mathias Chubrega <mc2geek@gmail.com>
2011-12-07 21:34:58 -0200

    actualizado

---
+++ dominio.py
@@ -0,0 +1,12 @@
+class dominio(object):
+
+    def __init__(self, alto, ancho)
+	self.alto = alto
+	self.ancho = ancho
+	self.listaBarreras = []
+	self.listaCondicionesExternas = []
+	print "Se ha creado el Dominio"
+
+    def __del__(self)
+        print "Se ha eliminado el Dominio"
+

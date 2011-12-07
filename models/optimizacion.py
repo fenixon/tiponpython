9f5ff373980800e61873241f422078b328b6f1f9
Sebastian Daloia <sebastiandaloia@gmail.com>
2011-12-07 21:16:24 -0200

    actualizacion de la clase optimizacion

--- models/optimizacion.py
+++ models/optimizacion.py
@@ -3,8 +3,11 @@ from numpy import *
 class optimizacion(object):
 
     def __init__(self):
-        self.MatrizDescenso = zeros( (3,1) )
+        self.matrizDescenso = zeros( (3,1) )
         self.listaParametros = []
 
     def __del__(self):
         print "Objeto eliminado"
+
+    def calcular():
+        print "Calculo de optimizacion"

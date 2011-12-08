6fad317613425169303699676d8cb6811cc94004
Alvaro Lastname <ajnamy@hotmail.com>
2011-12-07 22:06:23 -0200

    actualizado barrera.py

--- models/barrera.py
+++ models/barrera.py
@@ -1,8 +1,9 @@
 class barrera():
 
-	def __init__(self, x1, x2, y1, y2):
+	def __init__(self, x1, x2, y1, y2, tipo):
 
 		self.x1 = x1
 		self.x2 = x2
 		self.y1 = y1
 		self.y2 = y2
+		self.tipo = tipo

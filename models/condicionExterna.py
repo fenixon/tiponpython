40524f0631452212cea9401c23a6ebc12f900120
Satanastian <sebastiandaloia@gmail.com>
2012-01-10 07:40:44 -0200

    Revision de Diagrama de clases, modelo dominio, gantt

--- models/condicionExterna.py
+++ models/condicionExterna.py
@@ -3,7 +3,6 @@ class condicionExterna(object):
     def __init__(self, tipo, valor):
         self.tipo = tipo
         self.valor = valor
-        print "Se ha creado una Condicion Externa"
         
     def __del__(self):
         print "Se ha eliminado una Condicion Externa"
